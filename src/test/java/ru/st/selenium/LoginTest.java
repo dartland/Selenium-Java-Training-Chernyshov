1f6541fc7cfd3b5dd80d55442b3afeaedda81b7e
dartland <dartland@rambler.ru>
2015-06-07 15:09:47 +0300

    Login, Add and Remove new user

--- src/test/java/ru/st/selenium/LoginTest.java
+++ src/test/java/ru/st/selenium/LoginTest.java
@@ -2,10 +2,13 @@ package ru.st.selenium;
 
 import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
+
 import org.testng.*;
 import org.testng.annotations.*;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
+
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
@@ -16,11 +19,30 @@ public class LoginTest extends ru.st.selenium.pages.TestBase {
 
   @Test
   public void testUntitled() throws Exception {    driver.get(baseUrl + "/php4dvd/");
-    driver.findElement(By.id("username")).clear();
-    driver.findElement(By.id("username")).sendKeys("admin");
-    driver.findElement(By.name("password")).clear();
-    driver.findElement(By.name("password")).sendKeys("admin");
+    driver.manage().window().maximize();
+    WebElement usernameField = driver.findElement(By.id("username"));
+	usernameField.clear();
+    usernameField.sendKeys("admin");
+    WebElement passwordField = driver.findElement(By.name("password"));
+	passwordField.clear();
+    passwordField.sendKeys("admin");
     driver.findElement(By.name("submit")).click();
+    //add new user
+    driver.findElement(By.xpath("//a[contains(text(),'User management')]")).click();
+    driver.findElement(By.id("username")).clear();
+    driver.findElement(By.id("username")).sendKeys("dartland");
+    driver.findElement(By.name("email")).clear();
+    driver.findElement(By.name("email")).sendKeys("dartland@rambler.ru");
+    driver.findElement(By.id("password")).clear();
+    driver.findElement(By.id("password")).sendKeys("12345");
+    driver.findElement(By.id("password2")).clear();
+    driver.findElement(By.id("password2")).sendKeys("12345");
+    new Select(driver.findElement(By.name("permission"))).selectByVisibleText("Admin");
+    driver.findElement(By.cssSelector("option[value=\"2\"]")).click();
+    driver.findElement(By.name("submit")).click();
+    driver.findElement(By.cssSelector("img[alt=\"Remove\"]")).click();
+    assertTrue(closeAlertAndGetItsText().matches("^Are you sure you want to remove this[\\s\\S]$"));
+    Thread.sleep(5000);
   }
 
   private boolean isElementPresent(By by) {
